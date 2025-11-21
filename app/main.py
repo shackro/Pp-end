2080af1ed9d8bcbf14814bc9cabacf89875997a1
Shadrack <128532446+shackro@users.noreply.github.com>
2025-11-21 02:16:12 -0500

    Add specific origin to CORS configuration

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ app = FastAPI(
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*","https://pesaprime.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

e42b159c79b485f9c22b5734a72a293abe8d0289
Shadrack <128532446+shackro@users.noreply.github.com>
2025-11-25 00:34:53 -0500

    update

--- app/core/security.py
+++ app/core/security.py
@@ -28,3 +28,10 @@ def verify_token(token: str):
         return payload
     except JWTError:
         return None
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None

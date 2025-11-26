5bf332c39ff05fc0efbe60e1c6599a328c4302bb
Shadrack <128532446+shackro@users.noreply.github.com>
2025-11-26 01:36:32 -0500

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings:
     environment: str = os.getenv("ENVIRONMENT", "development")
     secret_key: str = os.getenv("SECRET_KEY", "5L5vfBJhjFPBGfMtXh_m5AjPVBXNTXCcPyqlYyJTsOU")
     algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
+    access_token_expire_minutes: int = 70
     
     # Database
     database_url: str = os.getenv("DATABASE_URL", "sqlite:///./PesaPrime.db")
